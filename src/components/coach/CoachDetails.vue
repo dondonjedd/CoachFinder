e4246aa4fe5c7772ed720348a8e22a3132086f5c
dondonj <48314079+dondonj@users.noreply.github.com>
2022-03-30 14:18:40 +0200

    link all the routes together

---
+++ src/components/coach/CoachDetails.vue
@@ -0,0 +1,5 @@
+<template>
+    Coach Details
+    <router-link to="/contact">Contatct the coach</router-link>
+    <router-view></router-view>
+</template>
--- src/components/coach/CoachesList.vue
+++ src/components/coach/CoachesList.vue
@@ -4,6 +4,10 @@
 </section>
 
 <section>
+    <div>
+        <button>Refresh</button>
+        <router-link to="/registration">Registration</router-link>
+    </div>
     Coaches List
 </section>
 </template>
