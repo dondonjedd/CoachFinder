e435f10b4ee2709c82427605e83374b50117beee
dondonj <48314079+dondonj@users.noreply.github.com>
2022-03-30 11:23:48 +0200

    set all routers

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{
+            path: "/",
+            redirect: "/coaches"
+        },
+        {
+            path: "/coaches",
+            component: null
+        },
+        {
+            path: "/coaches/:id",
+            component: null,
+            children: [{
+                path: "/contact",
+                component: null
+            }, ]
+        },
+        {
+            path: "/registration",
+            component: null
+        },
+        {
+            path: "/request",
+            component: null
+        },
+        {
+            path: "/:notFound(.*)",
+            component: null
+        },
+
+    ]
+})
