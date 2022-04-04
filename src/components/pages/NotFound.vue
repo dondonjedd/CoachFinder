a919187eed61d33115e7500db279f42949005e62
dondonj <48314079+dondonj@users.noreply.github.com>
2022-04-04 12:05:59 +0200

    create notFound page component

---
+++ src/components/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+        <base-card>
+        <h2>Page not found</h2>
+        <p>This page could not be found, go to this
+            <router-link to="/coaches">link</router-link> instead
+        </p>
+        </base-card>
+    </section>
+</template>
